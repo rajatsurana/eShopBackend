961824823e79e4b4ac20b26b16eaf83671eceb57
Robin Malhotra <malhotra.robin@icloud.com>
2016-02-19 23:52:08 +0530

    Surana y u do dis

--- app.js
+++ app.js
@@ -17,61 +17,77 @@ router.get('/', function(req, res) {
 });
 
 router.route('/products')
-    .get(function(req, res)
+.get(function(req, res)
+{
+    Product.find(function(err, products)
     {
-        Product.find(function(err, products)
+        if (err)
         {
-            if (err)
-            {
-                res.send(err)
-            }
+            res.send(err)
+        }
+        res.json(products);
+    });
+})
+.post(function(req,res)
+{
+    var product = new Product();
 
+    product.price =  req.body.price || '0',
+    product.quantity = req.body.quantity || '0',
+    product.description = req.body.description || 'default'
 
-            res.json(products);
-        });
-    })
-    .post(function(req,res)
+    product.save(function(err) {
+        if (err)
+        {
+            res.send(err);
+        }
+        res.json({ message: 'product created!', newProduct: product});
+    });
+})
+router.route('/update_price')
+.post(function(req, res)
+{
+
+    if (req.body["price"] == 0)
     {
-        var product = new Product();
+        res.send({message : "price can't be 0"})
+    }
+    else
+    {
+        Product.findOne({ _id: req.body.id }, function(err, product)
+        {
 
-            product.price =  req.body.price || '0',
-            product.quantity = req.body.quantity || '0',
-            product.description = req.body.description || 'default'
+            product.price = req.body.price;
 
+            product.save(function(err) {
+                if (err)
+                {
+                    res.send(err);
+                }
+                res.json({ message: 'Product price updated!' ,newProduct:product});
+            });
+
+        });
+    }
+
+});
+router.route('/change_discount')
+.post(function(req, res)
+{
+    Product.findOne({ _id: req.body.id }, function(err, product)
+    {
+        product.discount=req.body.discount || '0';
         product.save(function(err) {
             if (err)
-            res.send(err);
-
-            res.json({ message: 'product created!', newProduct: product});
+            {
+                res.send(err);
+            }
+            res.json({ message: 'Discount value changed!' ,newProduct : product});
         });
-    })
-router.route('/update_price')
-	.post(function(req, res) {
-		Product.findOne({ _id: req.body._id }, function(err, product) {
-			product.price=req.body.price || '0';
-			product.save(function(err) {
-				if (err)
-				res.send(err);
 
-				res.json({ message: 'Product price updated!' });
-			});
+    });
+});
 
-		});
-	});
-router.route('/change_discount')
-	.post(function(req, res) {
-		Product.findOne({ _id: req.body._id }, function(err, product) {
-			product.discount=req.body.discount || '0';
-			product.save(function(err) {
-				if (err)
-				res.send(err);
-
-				res.json({ message: 'Discount value changed!' });
-			});
-
-		});
-	});
-	
 
 app.use('/api', router);
 app.listen(3000);
