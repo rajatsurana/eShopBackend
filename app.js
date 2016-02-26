7261e1f3e2f2a84dc3387602cbfe51de3a801ff5
rajatsurana <rajat.surana123@gmail.com>
2016-02-27 00:11:09 +0530

    auth with tokens successfull

--- app.js
+++ app.js
@@ -8,16 +8,108 @@ var router = express.Router();
 var pushiPhone = require('./apns/pushiPhone')
 var pushAndroid = require('./androidPush/androidPNS')
 var async = require('async')
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var config = require('./models/config'); // get our config file
+var User   = require('./models/user'); // get our mongoose model
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost/eSubzi');
-
+app.set('superSecret', config.secret);
 
 router.get('/', function(req, res) {
     res.json({ message: 'hooray! welcome to our api!' });
 });
+router.route('/setup')
+.get(function(req, res) {
+  // create a sample user
+  var nick = new User({
+    name: 'Rajat Surana',
+    password: 'password',
+    admin: true
+  });
+  // save the sample user
+  nick.save(function(err) {
+    if (err) throw err;
 
+    console.log('User saved successfully');
+    res.json({ success: true ,user:nick});
+  });
+});
+router.get('/users', function(req, res) {
+  User.find({}, function(err, users) {
+    res.json(users);
+  });
+});
+router.post('/authenticate', function(req, res) {
+
+  // find the user
+  User.findOne({
+    name: req.body.name
+  }, function(err, user) {
+
+    if (err) throw err;
+
+    if (!user) {
+      res.json({ success: false, message: 'Authentication failed. User not found.' });
+    } else if (user) {
+
+      // check if password matches
+      if (user.password != req.body.password) {
+        res.json({ success: false, message: 'Authentication failed. Wrong password.' });
+      } else {
+
+        // if user is found and password is right
+        // create a token
+        var token = jwt.sign(user, app.get('superSecret'), {
+          expiresInMinutes: 1440 // expires in 24 hours
+        });
+
+        // return the information including token as JSON
+        res.json({
+          success: true,
+          message: 'Enjoy your token!',
+          token: token
+        });
+      }
+
+    }
+
+  });
+});
+//check token is present or not--middleware
+router.use(function(req, res, next) {
+
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.query.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+
+    // verifies secret and checks exp
+    jwt.verify(token, app.get('superSecret'), function(err, decoded) {
+      if (err) {
+        return res.json({ success: false, message: 'Failed to authenticate token.' });
+      } else {
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+        success: false,
+        message: 'No token provided.'
+    });
+
+  }
+});
+
+//token required before fetching products or other apis
 router.route('/products')
 .get(function(req, res)
 {
@@ -108,4 +200,3 @@ router.route('/change_discount')
 app.use('/api', router);
 app.listen(3000);
 console.log('Magic happens on port 3000');
-//56d09039e551f6281b399a6d
---
+++ models/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+
+    'secret': 'iloveSubzi',
+
+
+};
