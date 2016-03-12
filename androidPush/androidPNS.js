ac2727222d64848ad2c89c615b03de6ce8388b3d
rajatsurana <rajat.surana123@gmail.com>
2016-03-12 16:22:21 +0530

    api key updated

--- androidPush/androidPNS.js
+++ androidPush/androidPNS.js
@@ -10,7 +10,7 @@ function sendPushes(pushMessage,regArr)
 		  uri: "https://gcm-http.googleapis.com/gcm/send",
 		  method: "POST",
 		  headers: {
-		    'Authorization': 'key=AIzaSyA2cj6dAC3UTWlyANDkuxCb4_R7S4S5s6U',
+		    'Authorization': 'key=AIzaSyAWNh0qvyYpOP7hJC3Nkh6PJ9zj0ydPQfs',
 			'Content-type':'application/json'
 			},
 			json:
@@ -27,5 +27,5 @@ function sendPushes(pushMessage,regArr)
 	}
 }
 //"ekxywP1V6Z8:APA91bG9TZ8NsS7xjRqx2cXg0yy1MzdNvu0ypsiP0_25j98hyTRQi2IiayMVag_WyUEhcl35MqBh1a7GqPQz5bTjHLYLOZ-RJ6KLWhH_GjAG90oNQSDYF1vJe1SsTCGf-B2doxPk5FcA"
-
+//elhci1kOtGo:APA91bGIeLt1Fdqri-GGTb2aGz_I-Neq4ZWDRj4__O_BlR8D34Yuyz2HU8WKPxO2BzqZya19uPnWWmd1JRjHtDf4E80F5NnxWsb5q14yLp1UieimkTv3orI8FK0C3KPF-iARoROIZXmN
 module.exports.sendPushes = sendPushes
