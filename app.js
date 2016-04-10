fc91c3e614b1140bc17ce11477d9a32494946715
Robin Malhotra <malhotra.robin@yahoo.co.in>
2016-04-10 20:49:30 +0530

    asdfdsfa

--- app.js
+++ app.js
@@ -130,31 +130,31 @@ router.post('/signup', function(req, res, next)
 });
 
 
-router.use(function(req, res, next)
-{
-    var token = req.body.token || req.query.token || req.headers['x-access-token'];
-    if (token) {
-        // verifies secret and checks exp
-        jwt.verify(token, app.get(secret), function(err, decoded) {
-            if (err) {
-                return res.json({ success: false, message: 'Failed to authenticate token.' });
-            } else {
-                // if everything is good, save to request for use in other routes
-                req.decoded = decoded;
-                next();
-            }
-        });
-
-    } else {
-        // if there is no token
-        // return an error
-        return res.status(403).send({
-            success: false,
-            message: 'No token provided.'
-        });
-
-    }
-});
+// router.use(function(req, res, next)
+// {
+//     var token = req.body.token || req.query.token || req.headers['x-access-token'];
+//     if (token) {
+//         // verifies secret and checks exp
+//         jwt.verify(token, app.get(secret), function(err, decoded) {
+//             if (err) {
+//                 return res.json({ success: false, message: 'Failed to authenticate token.' });
+//             } else {
+//                 // if everything is good, save to request for use in other routes
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
+//
+//     } else {
+//         // if there is no token
+//         // return an error
+//         return res.status(403).send({
+//             success: false,
+//             message: 'No token provided.'
+//         });
+//
+//     }
+// });
 
 //token required before fetching products or other apis below
 router.route('/register')
@@ -352,6 +352,7 @@ router.route('/changeDiscount')
 router.route('/placeOrder')
 .post(function(req, res)
 {
+    console.log(req.body);
     var items = Object.keys(req.body.items)
     Product.find({ _id: { $in: items } }, function (err, result)
     {
